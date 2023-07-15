e2ab4ee8d388c693b0c1ee27829400b0f426bd9d
thegaab <tiagogabrielcontato@hotmail.com>
2023-07-15 20:45:30 -0300

    Chat-IA - 2023/07/15

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Chat } from "@/components/Chat";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen bg-slate-50 items-center justify-center">
+    <div className="flex min-h-screen bg-slate-50 items-center justify-center bg-gradient-to-r from-rose-100 to-teal-100">
       <Chat/>
     </div>
   )
